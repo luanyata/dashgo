5ea0eea64cb57fcc74c4606f5def7ab63add7979
Luan Lima <luan@yata.dev>
2021-04-18 12:42:29 -0300

    :sparkles:  Criacao da tela de login e componente Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
-export default function Home() {
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
   return (
-    <h1>DashGo</h1>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
